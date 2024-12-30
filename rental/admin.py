eae6a96d47eb40218de3267a535c43bd2a0ad3d6
Eugine Emoyo Okedi <okedieugine@gmail.com>
2024-12-30 14:03:56 +0300

    updated rental admin

--- rental/admin.py
+++ rental/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.site.register(Rental)
+class RentalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'car', 'user', 'start_date', 'end_date', 'status')
